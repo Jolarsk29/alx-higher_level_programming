e23e976da3f7235ac7f63c09003ab318c4ca61b7
Omojola Oluwatomisin <omojola1234@gmail.com>
2022-11-29 12:48:41 -0800

    mandatory task

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -3,12 +3,8 @@
 # Author - Oluwatomisin Omojola
 
 def islower(c):
-
     """Function checks for lowercase characters."""
-
     if ord(c) >= 97 and ord(c) <= 122:
-
         return True
-
     else:
         return False
