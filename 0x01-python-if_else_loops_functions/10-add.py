4aa422c2087ab2a08ed1e75ee4e0f84086c6f5cd
Omojola Oluwatomisin <omojola1234@gmail.com>
2022-11-29 10:57:44 -0800

    mandatory task

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+# Author - Oluwatomisin Omojola
+
+def add(a, b):
+
+    """Return the addition of a and b."""
+
---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# Author - Tolulope Fakunle
+
+
+
+def print_last_digit(number):
+
+    print(abs(number) % 10, end="")
+
