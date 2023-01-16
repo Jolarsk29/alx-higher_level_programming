ef8b7e0ec07461db169fd25f216f74026169e733
Omojola Oluwatomisin <omojola1234@gmail.com>
2023-01-16 03:38:32 -0800

    mandatory task

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""This module adds all arguments to a Python list and save them to a file."""
-
-
+"""Add all arguments to a Python list and save them to a file."""
 import sys
 
 if __name__ == "__main__":
@@ -15,5 +13,3 @@ if __name__ == "__main__":
         items = []
     items.extend(sys.argv[1:])
     save_to_json_file(items, "add_item.json")
-
-
